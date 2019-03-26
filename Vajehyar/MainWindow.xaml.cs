c4168e35fef8bb7029fa02e64680debf3f39adfa
Ayub <kokabi1365@gmail.com>
2019-03-26 16:59:20 +0430

    wpf.vajehyar

---
+++ Vajehyar/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Vajehyar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ICollectionView _words;
+        private string _filterString;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Words = CollectionViewSource.GetDefaultView(data());
+            Words.Filter = new Predicate<object>(Filter);
+
+
+        }
+
+        public class Word
+        {
+            public string Definition { get; set; }
+            public string Mean { get; set; }
+        }
+
+        public string FilterString
+        {
+            get => _filterString;
+            set
+            {
+                _filterString = value;
+                NotifyPropertyChanged("FilterString");
+                FilterCollection();
+            }
+        }
+
+        private void FilterCollection()
+        {
+            if (_words != null)
+            {
+                _words.Refresh();
+            }
+        }
+
+        public bool Filter(object obj)
+        {
+            Word data = obj as Word;
+            if (data != null)
+            {
+                if (!string.IsNullOrEmpty(_filterString))
+                {
+                    return data.Definition.Trim().Contains(_filterString);
+                }
+                return true;
+            }
+            return false;
+        }
+
+
+        public ICollectionView Words
+        {
+            get => _words;
+            set { _words = value; NotifyPropertyChanged("Words"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public List<Word> data()
+        {
+
+            string content = Properties.Resources.words;
+            string[] lines = content.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            List<Word> words = new List<Word>();
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(':');
+                Word word = new Word
+                {
+                    Definition = cols[0],
+                    Mean = cols[1]
+                };
+                words.Add(word);
+
+
+            }
+
+            return words;
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void TxtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterString = txtSearch.Text;
+
+        }
+    }
+}
