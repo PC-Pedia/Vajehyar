8e610337c64c289603f8721d17d725224a0f4b7a
Ayub <kokabi1365@gmail.com>
2019-04-24 19:17:05 +0430

    Improve hotkey textbox

--- Vajehyar/Windows/SettingWindow.xaml.cs
+++ Vajehyar/Windows/SettingWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace Vajehyar.Windows
 
             // Fetch the actual shortcut key.
             Key key = (e.Key == Key.System ? e.SystemKey : e.Key);
-
+           
             // Ignore modifier keys.
             if (key == Key.LeftShift || key == Key.RightShift
                                      || key == Key.LeftCtrl || key == Key.RightCtrl
@@ -60,9 +60,8 @@ namespace Vajehyar.Windows
             {
                 shortcutText.Append("Alt + ");
             }
-            shortcutText.Append(key.ToString());
 
-            // Update the text box.
+            shortcutText.Append(key.ToString());
             textBox.Text = shortcutText.ToString();
         }
 
@@ -83,6 +82,11 @@ namespace Vajehyar.Windows
                 Close();
                 e.Handled = true;
             }
+        }
+
+        private void TextBox_OnPreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            
         }
     }
 }
